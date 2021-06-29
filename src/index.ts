a3fd992cc6a6097965c9bab573409a8e8ce4feb6
Florian Koch <typingsytems@gmail.com>
2021-06-29 11:17:34 +0300

    Export default identifier keys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { SvelteGettextExtractor } from './extractor';
-export { callExpressionExtractor, ICustomJsExtractorOptions, TTranslatorFunction } from './js/extractors/factories/callExpression';
+export { callExpressionExtractor, ICustomJsExtractorOptions, TTranslatorFunction, defaultIdentifierKeys } from './js/extractors/factories/callExpression';
 export { FunctionExtractorBuilder, FunctionExtractor } from './js/extractors/functionExtractors';
 export { SvelteParser } from './svelte/parser';
 export { JsParser } from './js/parser';
