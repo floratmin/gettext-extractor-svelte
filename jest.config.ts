1bfee2ea321e2461dcc95513bfb74d5d495b8e76
Florian Koch <florat.min@gmail.com>
2023-10-05 08:31:50 +0200

    Update dependency

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,9 @@ import type { Config } from '@jest/types';
 
 export default <Config.InitialOptions>{
   transform: {
-    '.ts': 'ts-jest',
+    '.ts': ['ts-jest', { tsconfig: 'tests/tsconfig.json' }],
   },
   testRegex: '.*\\.test\\.ts$',
   roots: ['<rootDir>/tests'],
   moduleFileExtensions: ['js', 'ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tests/tsconfig.json',
-    },
-  },
 };
